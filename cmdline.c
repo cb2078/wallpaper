f2e05eb871ae7cb75bec05398bec66d14595d283
charlie <cb2078@york.ac.uk>
2024-02-29 17:19:53 +0000

    align columns in help string

--- cmdline.c
+++ cmdline.c
@@ -19,14 +19,13 @@ struct option {
 struct option options[] = {
 	{ .name = "border", .type = 'f', .val.f = 0.05, },
 	{ .name = "coefficient", .mode = VIDEO, .type = 's', .doc = "coefficient to change during the video, must have regex \"[xy][0-5]\"", },
-	{ .name = "colour", .type = 'c', },
-	{ .name = "duration", .mode = VIDEO, .type = 'd', .val.d = 40, },
+	{ .name = "colour", .type = 'c', .doc = "how to colour the attractor"},
+	{ .name = "duration", .mode = VIDEO, .type = 'd', .val.d = 40, .doc = "duration in seconds"},
 	{ .name = "end", .mode = VIDEO, .type = 'f', .doc = "end value for coefficient," },
 	{ .name = "fps", .mode = VIDEO, .type = 'd', .val.d = 24, },
 	{ .name = "height", .type = 'd', .val.d = 720, },
 	{ .name = "intensity", .type = 'f', .val.f = 50, .doc = "how bright the iterations make the pixel"},
-	{ .name = "params", .type = 's', .doc = "file containing a set of 12 parameters on each line,\n"
-	                                        "parameters separated floats formatted with \"% 1.3f\""},
+	{ .name = "params", .type = 's', .doc = "file containing lines of 12 space separated floats"},
 	{ .name = "preview", .type = 'd', .doc = "show grid of some thumbnails" },
 	{ .name = "quality", .type = 'd', .val.d = 25, .doc = "how many iterations to do per pixel" },
 	{ .name = "start", .mode = VIDEO, .type = 'f', .doc = "start value for coefficient," },
@@ -61,11 +60,20 @@ static void help_mode(char *name, enum option_mode mode)
 
 static void help(void)
 {
+	int indent = 0;
+	for (int i = 0; i < LENGTH(options); ++i) {
+		char buf[256];
+		int c = snprintf(buf, 256, "  --%s %s", options[i].name, type_str(options[i].type));
+		indent = MAX(c, indent);
+	}
+	printf("%d\n", indent);
+
 	printf("usage\n");
 	help_mode("image", IMAGE);
 	help_mode("video", VIDEO);
 	printf("\noptions\n");
-	// TODO indentation
-	for (int i = 0; i < LENGTH(options); ++i)
-		printf("  --%s %s\t%s\n", options[i].name, type_str(options[i].type), options[i].doc ? options[i].doc : "");
+	for (int i = 0; i < LENGTH(options); ++i) {
+		int c = printf("  --%s %s", options[i].name, type_str(options[i].type));
+		printf("%*s  %s\n", indent - c, "", options[i].doc ? options[i].doc : "");
+	}
 }
